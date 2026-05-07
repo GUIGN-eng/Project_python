11581944ab301206ad33535c32ccc4a7a446259d
GUIGN-eng <guign1610@gmail.com>
2026-05-07 09:13:36 -0300

    Add files via upload

---
+++ thon_py/pyton/exercicio_def.py
@@ -0,0 +1,43 @@
+import os
+os.system("cls")
+
+#Calculo Fatorial com def
+'''def calcular_fatorial(n):
+    resul = 1
+    for cont in range(1, n + 1):
+        resul *= cont
+    return resul
+
+nf = int(input("Digite um número: "))
+
+print(f"O resultado do número em fatorial {calcular_fatorial(nf)}")'''
+
+#Calculo Fatorial simples
+'''print(".....Calculo_Fatorial.....")
+nf = int(input("Digite um número: "))
+
+calcular_fatorial = 1
+for cont in range(1, nf + 1):
+    calcular_fatorial *= cont
+
+print(f"Fatorial: {calcular_fatorial}")'''
+
+#Calculo de Potencia com def
+'''def calcular_potencia(n1, n2):
+    potencia = n1 ** n2
+    return potencia
+
+print("Calculo de potencia")
+base = int(input("Dite o número base: "))
+expoente = int(input("Digite o número expoente: "))
+
+print(f"Potencia = {calcular_potencia(base, expoente)}")'''
+
+#Calculo de Potencia simples
+'''print(".....Calculo de potencia.....")
+base = int(input("Digite o número base: "))
+expoente = int(input("Digite o número expoente: "))
+
+potencia = base ** expoente
+
+print(f"Potencia = {potencia}")'''
