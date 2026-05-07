5acaa1490d2121da9851098086de93c4633c0d2f
GUIGN-eng <guign1610@gmail.com>
2026-05-07 01:43:09 -0300

    Update exemplo01.py

--- thon_py/exemplo01.py
+++ thon_py/exemplo01.py
@@ -1,12 +1,11 @@
-#Corrigir
+import os
+os.system("cls")
 
-nome1 = str(input("Digite seu nome: "))
-nome2 = str(input("Digte seu nome: "))
-n1 = int(input("Digite sua idade: "))
-n2 = int(input("Digite sua idade: "))
-if (nome1 != "Guilherme" and nome2 != "Matheus") or (n1 < 18 and n2 < 18):
- if (nome1 == "Guilherme" and nome2 != "Matheus") or (n1 >= 18 or n2 < 18):
-  if (nome1 != "Guilherme" and nome2 == "Matheus") or (n1 < 18 or n2 >= 18):
-         print("\033[36;01mNOME E IDADE NÃO RECONHECIDO PROTOCOLO DE SEGURANÇA ATIVADO\033[M")
-elif(nome1 == "Guilherme" and nome2 == "Matheus") and (n1 >= 18 and n2 >= 18):
-    print("USUARIOS RECONHECIDOS. BEM-VINDOS BIG BROTHERS")
+nome1 = str(input("Digite o nome do host: "))
+nome2 = str(input("Digite o nome do subhost: "))
+n1 = int(input("Digite a idade do host: "))
+n2 = int(input("Digite a idade do subhost: "))
+if nome1 != "Guilherme" or nome2 != "Matheus" and n1 < 18 or n2 < 18:
+    print("HOSTS NÃO DETECTADOS.....ATIVANDO PROTOCOLO ARMAGEDON")
+else:
+    print(f"Bem-Vindos {nome1} e {nome2}, Garotos de Programa")
