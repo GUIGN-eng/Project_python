4baa6959e3f60ca3c9fb050bf5fe8363b28b82c1
GUIGN-eng <guign1610@gmail.com>
2026-05-07 01:38:51 -0300

    exercicio.py

--- thon_py/pyton/exercicio.py
+++ thon_py/pyton/exercicio.py
@@ -59,12 +59,70 @@ while n != 0:
 #''')
 
 #6.
-while True:
+'''while True:
     n = int(input("Digite um número: "))
     if n == 10:
+        print("Parabéns Você Acertou!")
         break
     if n >= 1 and n <= 5:
         print("Não está proximo!")
-    elif n >= 6 and n <= 7:
-        print("Está proximo")
-    elif
+    elif n >= 6 and n <= 8:
+        print("Está proximo!")
+    elif n == 9:
+        print("Falta só mais um")
+    else:
+        print("Passou longe!")'''
+
+#7.
+'''login_correto = "Host"
+senha_correta = "1234"
+
+tentativas = 0
+
+while tentativas < 3:
+    login = input("Imforme seu login: ")
+    senha = input("Imforme sua senha: ")
+    if login == login_correto and senha == senha_correta:
+        print("Login e Senha validos. Bem-Vindo!")
+        break
+    else:
+        tentativas += 1
+        print("Login e senha invalidos!")
+if tentativas == 3:
+    print("Limite de tentativas alcançado.")'''
+
+#8.
+saldo = 5000
+while True:
+    print('''
+       1-Sacar
+       2-Depositar
+       3-Consultar saldo
+       Exit-Sair
+    ''')
+    opcao = input("\nDigite uma das opções: ").title()
+    match opcao:
+        case "1":
+            print("\nQuanto gostaria de sacar?")
+            sacar = float(input("Saque: R$"))
+            if sacar > 0:
+                if sacar <= saldo:
+                    saldo -= sacar
+                else:
+                    print("\nSaldo invalido!")
+            else:
+                print("\nO valor tem que ser maior que 0")
+        case "2":
+            print("\nQuanto gostaria de depositar?")
+            depositar = float(input("Deposito: R$"))
+            if depositar > 0:
+                saldo += depositar
+                print(f"\nSeu deposito no valor de R${depositar} foi realizado com sucesso!")
+            else:
+                print("\nO valor tem que ser maior que 0")
+        case "3":
+            print(f"\nSeu Saldo é de: R${saldo}")
+        case "Exit":
+            break
+        case _:
+            print("\nOpção digitada invalido. Por favor digite de 1 a 3 ou Exit")
