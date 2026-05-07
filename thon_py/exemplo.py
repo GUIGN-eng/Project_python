3bed2b99e3109754d90af0669f9b433b54f93e86
GUIGN-eng <guign1610@gmail.com>
2026-05-07 01:41:00 -0300

    Update exemplo.py

--- thon_py/exemplo.py
+++ thon_py/exemplo.py
@@ -7,7 +7,7 @@ ano_nascimento = int(input("Informe o ano em que voçê nasceu: "))
 idade = 2026 - ano_nascimento
 
 if nome == "Guilherme" and idade == 21:
-    print("GOSTOÃO DO HOST RECONHECIDO")
+    print("GOSTOSÃO DO HOST RECONHECIDO")
 else:
     print("Deixa de tentar vagabunda, não te conheço" \
     "\nEntão voçê não entra")
